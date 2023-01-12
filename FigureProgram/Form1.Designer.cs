05b5e306a0a04306901fe0771ad7d3b00a662203
IvailoA05 <ivajloarabadziev@gmail.com>
2023-01-12 13:42:49 +0200

    The program now can draw rectangles

--- FigureProgram/Form1.Designer.cs
+++ FigureProgram/Form1.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRec = new System.Windows.Forms.Button();
+            this.btnTrg = new System.Windows.Forms.Button();
+            this.btnCrc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRec
+            // 
+            this.btnRec.Location = new System.Drawing.Point(12, 12);
+            this.btnRec.Name = "btnRec";
+            this.btnRec.Size = new System.Drawing.Size(94, 29);
+            this.btnRec.TabIndex = 0;
+            this.btnRec.Text = "Rectangle";
+            this.btnRec.UseVisualStyleBackColor = true;
+            this.btnRec.Click += new System.EventHandler(this.btnRec_Click);
+            // 
+            // btnTrg
+            // 
+            this.btnTrg.Location = new System.Drawing.Point(123, 12);
+            this.btnTrg.Name = "btnTrg";
+            this.btnTrg.Size = new System.Drawing.Size(94, 29);
+            this.btnTrg.TabIndex = 1;
+            this.btnTrg.Text = "Triangle";
+            this.btnTrg.UseVisualStyleBackColor = true;
+            this.btnTrg.Click += new System.EventHandler(this.btnTrg_Click);
+            // 
+            // btnCrc
+            // 
+            this.btnCrc.Location = new System.Drawing.Point(234, 12);
+            this.btnCrc.Name = "btnCrc";
+            this.btnCrc.Size = new System.Drawing.Size(94, 29);
+            this.btnCrc.TabIndex = 2;
+            this.btnCrc.Text = "Circle";
+            this.btnCrc.UseVisualStyleBackColor = true;
+            this.btnCrc.Click += new System.EventHandler(this.btnCrc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCrc);
+            this.Controls.Add(this.btnTrg);
+            this.Controls.Add(this.btnRec);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button btnRec;
+        private Button btnTrg;
+        private Button btnCrc;
     }
 }
--- FigureProgram/Form1.cs
+++ FigureProgram/Form1.cs
@@ -1,3 +1,5 @@
+using System.Threading;
+
 namespace FigureProgram
 {
     public partial class Form1 : Form
@@ -5,6 +7,60 @@ namespace FigureProgram
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        Thread th;
+        Thread th1;
+        Thread th2;
+        private void btnRec_Click(object sender, EventArgs e)
+        {
+            th = new Thread(threadr);
+            th.Start();
+        }
+        
+        public void threadr()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                this.CreateGraphics().DrawRectangle(new Pen(Brushes.Red, 4), new Rectangle(new Random().Next(0, this.Width),new Random().Next(0, this.Height), 20, 20));
+                Thread.Sleep(3000);
+            }
+            MessageBox.Show("Rectangles are ready!");
+        }
+
+        private void btnTrg_Click(object sender, EventArgs e)
+        {
+            th1 = new Thread(threadt);
+            th1.Start();
+        }
+
+        public void threadt()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(2000);
+            }
+            MessageBox.Show("Triangles are ready!");
+        }
+
+        private void btnCrc_Click(object sender, EventArgs e)
+        {
+            th2 = new Thread(threadc);
+            th2.Start();
+        }
+
+        public void threadc()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(4000);
+            }
+            MessageBox.Show("Circles are ready!");
         }
     }
 }
